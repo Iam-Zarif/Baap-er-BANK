e10b320bf0f39571dddb4fbac4cd73a6448e40c8
Zarif <zarifraja19@gmail.com>
2023-02-12 18:34:31 +0600

    added

---
+++ depo.js
@@ -0,0 +1,47 @@
+// document.getElementById('btn-1').addEventListener('click',function(){
+//     let input = document.getElementById("depo");
+// let inputValue = input.value;
+//     // console.log(inputValue);
+
+//     let depoTotal = document.getElementById("depo-total");
+//     // console.log(depoTotal.innerText);
+//     depoTotal.innerText = inputValue;
+
+//     input.value = '';
+// })
+
+
+document.getElementById("btn-1").addEventListener("click", function () {
+  let input = document.getElementById("depo");
+  let newInputValue = input.value;
+  // console.log(inputValue);
+
+  let depoTotal = document.getElementById("depo-total");
+  // console.log(depoTotal.innerText);
+  let previuousDepoTotal = Number(depoTotal.innerText);
+  depoTotal.innerText = Number(previuousDepoTotal)+Number(newInputValue);
+//   
+let balance = document.getElementById("balance");
+let previousBalance = Number(balance.innerText);
+balance.innerText = Number(previousBalance)+ Number(newInputValue);
+
+  input.value = "";
+});
+
+document.getElementById('btn-2').addEventListener('click',function(){
+    let input = document.getElementById("with");
+    let inputValue = input.value;
+    // console.log(inputValue);
+    // 
+
+    let withdraw = document.getElementById("withdraw");
+    let withdrawAmount = Number(withdraw.innerText);
+    withdraw.innerText = withdrawAmount + Number(inputValue);
+
+    // 
+
+    let balance = document.getElementById("balance");
+    let balanceAmount = Number(balance.innerText);
+    balance.innerText = balanceAmount - Number(inputValue);
+
+})
--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 
-document.getElementById('btn-1').addEventListener('click', function(){
+// document.getElementById('btn-1').addEventListener('click', function(){
   
-    let input =document.getElementById('email');
-    let email = input.value;
-     console.log(email);
+//     let input =document.getElementById('email');
+//     let email = input.value;
+//      console.log(email);
 
-    let pass =document.getElementById('pass');
-    let finalPass = pass.value;
-    // console.log(finalPass);
+//     let pass =document.getElementById('pass');
+//     let finalPass = pass.value;
+//     console.log(finalPass);
 
-    if(email === 'zarif@gmail.com' && finalPass === 'secret'){
-        window.location.href = "./bank.html";
-    }
-    else{
-        alert("Haha Gotcha");
-    }
+//     if(email === 'zarif@gmail.com' && finalPass === 'secret'){
+//         window.location.href = "bank.html";
+//     }
+//     else{
+//         alert("Haha Gotcha");
+//     }
 
-})
+// })
+
+
+
+document.getElementById("btn-1").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let input = document.getElementById("email");
+  let pass = document.getElementById("pass");
+  let email = input.value;
+  let finalPass = pass.value;
+  // console.log(finalPass);
+
+  if (email === "zarif@gmail.com" && finalPass === "secret") {
+    window.open("./bank.html");
+  } else {
+    alert("Haha Gotcha");
+  }
+});
